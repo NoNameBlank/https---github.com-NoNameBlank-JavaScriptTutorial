b50d1c6527d0b5e54d374f9b00bf8513a7693f76
NoNameBlank <marekphilipp@gmx.de>
2023-09-20 16:13:22 +0200

    Document Object Model Part 1

---
+++ Document Object Model 1/Das Document-Object/main.js
@@ -0,0 +1,10 @@
+"use strict";
+
+console.log(document);
+
+console.log(document.head);
+console.log(document.body);
+console.log(document.location);
+//console.log(document.location.href);
+//console.log(document.location.href = "https://www.google.de");
+console.log(document.URL); // Schreibgeschütze Eigenschaft, das die URL nicht verändert werden kann. (Ist sonst wie document.location.href)
