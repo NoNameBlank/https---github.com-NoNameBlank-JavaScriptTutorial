e95399d76ff7919b4bc39d51ea167846c4726deb
NoNameBlank <marekphilipp@gmx.de>
2023-09-22 20:16:53 +0200

    Formular-Events

--- Document Object Model 2/Formular-Events/main.js
+++ Document Object Model 2/Formular-Events/main.js
@@ -3,7 +3,7 @@
 let formular = document.querySelector("form");
 
 formular.addEventListener("submit", e => {
-    e.preventDefault();
+     e.preventDefault(); // Schnapp dir das Event und verhindere das das Standard verhalten ausgeführt wird
     console.log(e);
 });
 
